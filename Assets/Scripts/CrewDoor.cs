9ba97c7a4f8ab56853847586986c92c50efa46ea
davijoh73 <davetjohnston@hotmail.com>
2017-11-19 21:50:53 -0600

    feat: Finished building crew quarters and started on the engine room. Also added signs to help user understand where they are on the ship, as well as give hints about what to do to progress through the game.

---
+++ Assets/Scripts/CrewDoor.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrewDoor : MonoBehaviour 
+{
+    //The locked variable is used in the Unlock method to "unlock" the door
+    public bool locked = false;
+    //Trigger used to open the door once it's unlocked
+    public bool openDoor = false;
+    //Sound clips used for when the door is locked, and when it's opened
+    public GvrAudioSource soundSource;
+    public AudioClip doorLocked;
+    public AudioClip doorOpen;
+
+    //Sections of the door that need to open together
+    public GameObject rightSide, leftSide;
+
+    void Update() {
+        //If door is unlocked and not fully raised, continue raising door
+        if (openDoor && rightSide.transform.position.z < 1.5f)
+        {
+            rightSide.transform.Translate(0, 0, 0.5f * Time.deltaTime, Space.World);
+            leftSide.transform.Translate(0, 0, -0.5f * Time.deltaTime, Space.World);
+        }
+    }
+
+    public void OnDoorClicked() {
+        if (locked == false)
+        {
+            //Trigger the door to open via animation script and play opening door sound
+            openDoor = true;
+            soundSource.clip = doorOpen;
+            soundSource.Play();
+        }
+        else
+        {
+            //Play locked door audio clip
+            soundSource.clip = doorLocked;
+            soundSource.Play();
+        }
+    }
+
+    public void Unlock()
+    {
+        //Call this method to unlock the door
+        locked = false;
+    }
+}
